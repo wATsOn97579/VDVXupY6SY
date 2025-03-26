87505f582adff40e667056987bf03f8c68b66d06
wATsOn97579 <sambas.coho_0s@icloud.com>
2025-03-26 22:27:59 +0000

    Document the file's configuration options

--- U5ia0/S0BB0/mXb/7Bh/microsoft-teams_pqG7_1742896787.md.cmd
+++ U5ia0/S0BB0/mXb/7Bh/microsoft-teams_pqG7_1742896787.md.cmd
@@ -3,7 +3,6 @@
 WHERE node >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 ECHO "node not found. please install node." 
 
-WHERE mkdocs >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 ECHO "mkdocs not found. please install python & mkdocs." 
 
 SET CURDIR=%~dp0
